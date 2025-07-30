029fc6bc4530a463ebf1295c13f706ecb04e482f
Unathi Myeni <0410295069083@tpc.local>
2025-07-30 10:30:58 +0200

    Shell file edits

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+mkdir Project-Navigator
 touch Project-Purpose.txt
 echo "-This project's purpose is to set up an effective project environment in which I document my process through the use of version control and integrating my work with a remote GitHub project by utilising a forked workflow " > Project-Purpose.txt
 echo " -Name of individual responsible for this project: Thando Myeni " >> Project-Purpose.txt
